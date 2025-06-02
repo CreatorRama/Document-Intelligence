187cae8bd3a38051c100d5e991d5dce1258776f4
Creator Rama <ap410485@gmail.com>
2025-06-02 18:26:57 +0530

    ui

--- frontend/docintel-frontend/src/app/qa/page.tsx
+++ frontend/docintel-frontend/src/app/qa/page.tsx
@@ -1,9 +1,15 @@
+'use client' 
+
+import { Suspense } from 'react'
 import QASection from './components/QASection'
+import LoadingSpinner from '../components/LoadingSpinner'
 
 export default function QAPage() {
   return (
     <div className="space-y-8">
-      <QASection />
+      <Suspense fallback={<LoadingSpinner />}>
+        <QASection />
+      </Suspense>
     </div>
   )
 }
