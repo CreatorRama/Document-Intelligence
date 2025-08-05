17e74d2d01d39cb954c2dc6b7dbc87793141e300
Creator Rama <ap410485@gmail.com>
2025-08-05 09:49:18 +0530

    wert

--- backend/build.sh
+++ backend/build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -o errexit
+set -o pipefail
 
-# Install system dependencies for psycopg
+# System dependencies
 apt-get update && apt-get install -y \
     libpq-dev \
     python3-dev \
     gcc
 
 # Install Python packages
+pip install --upgrade pip
 pip install -r requirements.txt
